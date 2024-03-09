34bbbfd504a6055f95589a1ffc33f3f801e390ff
Duy Phan <phanjuy@hotmail.com>
2024-03-09 13:58:19 +0100

    Configure MassTransit

--- MassTransit.InMemory/Program.cs
+++ MassTransit.InMemory/Program.cs
@@ -1,7 +1,15 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(cfg =>
+{
+    cfg.SetKebabCaseEndpointNameFormatter();
+    cfg.UsingInMemory((context, config) => config.ConfigureEndpoints(context));
+});
 
 var app = builder.Build();
 
